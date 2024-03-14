35821b9ee1b624734a847f0a147f0453abb991d4
aab-dii <mabdillahabdi600@gmail.com>
2024-03-14 13:16:01 +0800

    Day 4

---
+++ Day4/Casting/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class App {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       char a = read.next().charAt(0);
+       //your code goes here
+       int b = (int) a;
+       System.out.println(b);
+       read.close();
+    }   
+}
